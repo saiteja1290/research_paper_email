13cb4ab949f8ca2d7cbc6edb081cd85b5d66854c
TEJA <39546736+saiteja1290@users.noreply.github.com>
2023-06-20 17:34:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,38 +7,8 @@ import re
 import random
 
 app = Flask(__name__)
-def link_to_pdfs_and_titles(domain, subdomain, page = random.randint(1,500)):
-    domain = domain.replace(" ", "+")
-    subdomain = subdomain.replace(" ", "+")
-    titles = []
-    titles2 = []
-    pdf_links = []
-    # url = f"https://link.springer.com/search/page/{page}?facet-language=%22En%22&facet-sub-discipline=%22{subdomain}%22&facet-content-type=%22ConferencePaper%22&sortOrder=newestFirs&facet-discipline=%22{domain}%22"
-    # https://link.springer.com/search/page/2?facet-discipline=%22Computer+Science%22&facet-sub-discipline=%22Artificial+Intelligence%22&facet-content-type=%22ConferencePaper%22&facet-language=%22En%22
-    url = f"https://link.springer.com/search/page/{page}?facet-language=%22En%22&showAll=false&facet-content-type=%22ConferencePaper%22&facet-sub-discipline=%22{subdomain}%22&facet-discipline=%22{domain}%22"
-    response = requests.get(url)
-    html_content = response.content
 
-
-    soup = BeautifulSoup(html_content, "html.parser")
-
-
-    target_links = soup.find_all("a", class_="webtrekk-track pdf-link")
-    # target_links_title = soup.find_all("a", class_="title")
-    for link in target_links:
-        if(link.get("doi")):    
-            # titles = link.text
-            href = link.get("doi")
-            title = link.get("aria-label")
-            pdf_links.append("https://link.springer.com//content/pdf/"+href)
-            titles.append(title)
-    print(pdf_links)
-    # print(title)
-    for title in titles:
-        modified_text = re.sub(r"Download PDF \(\d+ KB\) - ", "", title)
-        # title = modified_text
-        titles2.append(modified_text)
-    print(titles2)
+# This function saves the data in excel sheet
 def save_to_excel(name, interests, email, subdomain):
     excel_file = 'registrations.xlsx'
     
